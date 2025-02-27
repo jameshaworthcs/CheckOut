4f00c7811d5cb9ef07e42b8cdd6e3f39baf90309
James Haworth <james@jameshaworth.dev>
2025-02-27 02:28:26 +0000

    feat: Enhance CORS configuration with specific origins and headers

--- index.ts
+++ index.ts
@@ -510,7 +510,10 @@ app.use((req: AppRequest, res: Response, next: NextFunction): void => {
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081, https://*.checkout.ac');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-checkout-key, User-Agent');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
